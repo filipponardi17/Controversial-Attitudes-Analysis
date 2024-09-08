06b481ac5ee3f0ea7291cc54d84fee7cdf557ef0
filipponardi17 <103837233+filipponardi17@users.noreply.github.com>
2024-09-08 15:33:22 +0200

    Initial commit

---
+++ conv_in_csv.R
@@ -0,0 +1,4 @@
+setwd("/Users/filipponardi/Documents/GitHub/CSS")
+load("/Users/filipponardi/Documents/GitHub/pop_vote/1 Dataframes imputati/Imputed PMM integral FINALE.Rda")
+write.csv(W7R,
+          file="yourCSV.csv")
